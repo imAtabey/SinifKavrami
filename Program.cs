159159745c9fa8bdb881e0eec756f698d34076b5
Hüseyin ÜTEBAY <huseyin.utebay@gmail.com>
2025-08-04 10:53:33 +0300

    Yapıcı Metot (Constructor) konusu eklendi

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-#region Sınıf Kavramı
-// Söz Dizimi
+#region Sinif Kavrami
+//-> Söz Dizimi
 // [Erişim Belirleyici] class [Sınıf Adı]
 // {
+//    public [Sınıf Adı]
+//    {
+//      Constructor Komutları
+//    }
 //    [Erişim Belirleyici] [Veri Tipi] [Özellik Adı]
 //    [Erişim Belirleyici] [Geri Dönüş Tipi] [Metotadı](Parametre Listesi)
 //    {
-//Metot Komutları
+//      Metot Komutları
 //    }
 // }
 
@@ -16,22 +20,31 @@
 // internal -> Kendi proje içerisinde erişilebilir
 // Protected -> Tanımlandığı sınıf veya o sınıftan kalıtım alan sınıflarda
 
-Calisan c1 = new Calisan();
-c1.Ad = "Ayşe";
-c1.Soyad = "Kara";
-c1.No = 23415634;
-c1.Departman = "İnsan Kaynakları";
+#endregion
+#region Erisim Belirleyici ve Constructor
+// Bir sınıfın nesnesi oluşturulunca arka planda çalışan metotlardır
+// Mutlaka public olmalılar
+// Geri dönüş tipleri yazılmaz
+// Mutlaka ilgili sınıfla aynı isimde olmalıdır.
+// Overload edilebilir.
+#endregion
+
+Console.WriteLine("*** Constructor'a parametre verilerek nesne oluşturma ***");
+Calisan c1 = new Calisan("Ayşe","Kara",23415634,"İnsan Kaynakları");
 c1.CalisanBilgileri();
 
+Console.WriteLine("*** Parametresiz Constructor ile oluşturulan Nesne ***");
 Calisan c2 = new Calisan();
 
 c2.Ad = "Deniz";
 c2.Soyad = "Arda";
 c2.No = 25646789;
 c2.Departman = "Satın Alma";
-
 c2.CalisanBilgileri();
 
+Console.WriteLine("*** Sadece Ad ve Soyad Parametresi olan Constructor ile Nesne Tanımlama");
+Calisan c3 = new Calisan("Hüseyin", "ÜTEBAY");
+c3.CalisanBilgileri();
 
 
 class Calisan
@@ -40,6 +53,22 @@ class Calisan
     public string? Soyad;
     public int No;
     public string? Departman;
+    public Calisan()
+    {
+
+    }
+    public Calisan(string ad, string soyad)
+    {
+        this.Ad = ad;
+        this.Soyad = soyad;
+    }
+    public Calisan(string ad, string soyad, int no, string departman)
+    {
+        this.Ad = ad;
+        this.Soyad = soyad;
+        this.No = no;
+        this.Departman = departman;
+    }
     public void CalisanBilgileri()
     {
         Console.WriteLine("Çalışan Adı :{0}", Ad);
@@ -50,4 +79,3 @@ class Calisan
 
 }
 
-#endregion
--- obj/Debug/net9.0/SinifKavrami.AssemblyInfo.cs
+++ obj/Debug/net9.0/SinifKavrami.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SinifKavrami")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc273224cff847e500410985e45bf12821e7413a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SinifKavrami")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SinifKavrami")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
