0362418ce4217c7a5c37e16a761644b69cbe228b
Hüseyin ÜTEBAY <huseyin.utebay@gmail.com>
2025-08-04 11:53:55 +0300

    Encapsulation Konusu Eklendi

--- Program.cs
+++ Program.cs
@@ -28,23 +28,45 @@
 // Mutlaka ilgili sınıfla aynı isimde olmalıdır.
 // Overload edilebilir.
 #endregion
+#region Encapsulation ve Property Kavrami
+// Nesnenin bazı özellik veya işlevlerini diğer class ve nesnelerden korumak için erişim belirleyiciler kullanılır
+// Private belirlenmiş bir değere dışarıdan ulaşmak ve ilgili property ve field'a müdahale ederken private yapıyı bozmadan oluşturulan yöntemdir.
+// Ogrenci sınıfı ile örneklendirildi.
 
+#endregion
+
+// -> Constructor Örneği [Parametreli]
 Console.WriteLine("*** Constructor'a parametre verilerek nesne oluşturma ***");
 Calisan c1 = new Calisan("Ayşe","Kara",23415634,"İnsan Kaynakları");
 c1.CalisanBilgileri();
-
+// -> Constructor Örneği 
 Console.WriteLine("*** Parametresiz Constructor ile oluşturulan Nesne ***");
 Calisan c2 = new Calisan();
-
 c2.Ad = "Deniz";
 c2.Soyad = "Arda";
 c2.No = 25646789;
 c2.Departman = "Satın Alma";
 c2.CalisanBilgileri();
-
+// -> Constructor Örneği [string ad,string soyad] parametreleri ile 
 Console.WriteLine("*** Sadece Ad ve Soyad Parametresi olan Constructor ile Nesne Tanımlama");
 Calisan c3 = new Calisan("Hüseyin", "ÜTEBAY");
 c3.CalisanBilgileri();
+
+
+//-> Encapsulation Örneği
+Console.WriteLine("*** Encapsulation Örneği ***");
+Ogrenci o1 = new Ogrenci();
+o1.Isim = "Ayşe";
+o1.Soyisim = "Yılmaz";
+o1.OgrenciNo = 293;
+o1.Sinif = 3;
+o1.SinifAtlat();
+o1.OgrenciBilgileriGetir();
+
+Ogrenci o2 = new("Deniz", "Arda", 256, 1);
+o2.SinifDusur();
+o2.SinifDusur();
+o2.OgrenciBilgileriGetir();
 
 
 class Calisan
@@ -78,4 +100,57 @@ class Calisan
     }
 
 }
+class Ogrenci
+{
+    private string _isim;
+    private string _soyisim;
+    private int _ogrenciNo;
+    private int _sinif;
 
+    public string Isim { get => _isim; set => _isim = value; }
+    public string Soyisim { get => _soyisim; set => _soyisim = value; }
+    public int OgrenciNo { get => _ogrenciNo; set => _ogrenciNo = value; }
+    public int Sinif
+    {
+        get => _sinif;
+        set
+        {
+            if (value <= 1)
+            {
+                Console.WriteLine("Sınıf en az bir olabilir");
+                Sinif = 1;
+            }
+            else
+                _sinif = value;
+        }
+    }
+
+    public Ogrenci(string isim, string soyisim, int ogrenciNo, int sinif)
+    {
+        Isim = isim;
+        Soyisim = soyisim;
+        OgrenciNo = ogrenciNo;
+        Sinif = sinif;
+    }
+    public Ogrenci()
+    {
+
+    }
+    public void OgrenciBilgileriGetir()
+    {
+        Console.WriteLine("*** Öğrenci Bilgileri ***");
+        Console.WriteLine("Öğrenci Adı :{0}", this.Isim);
+        Console.WriteLine("Öğrenci Adı :{0}", this.Soyisim);
+        Console.WriteLine("Öğrenci Adı :{0}", this.OgrenciNo);
+        Console.WriteLine("Öğrenci Adı :{0}", this.Sinif);
+    }
+    public void SinifAtlat()
+    {
+        this.Sinif = this.Sinif +1;
+    }
+    public void SinifDusur()
+    {
+        this.Sinif = this.Sinif -1;
+    }
+
+}
